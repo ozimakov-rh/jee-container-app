a47d0fc2bb77724d200bba799b10e3b10e974818
Oleg Zimakov <ozimakov@redhat.com>
2022-12-22 15:38:15 -0800

    Removes an unneccessary servlet method

--- user-webapp/src/main/java/com/redhat/demo/user/KudosServlet.java
+++ user-webapp/src/main/java/com/redhat/demo/user/KudosServlet.java
@@ -17,11 +17,6 @@ public class KudosServlet extends HttpServlet {
     private KudosService kudosService;
 
     @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        resp.getWriter().println(kudosService.listAllKudos().toString());
-    }
-
-    @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         String from = req.getParameter("from") != null ? req.getParameter("from") : "N/A";
         String to = req.getParameter("to") != null ? req.getParameter("to") : "N/A";
